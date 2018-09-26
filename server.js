0e2e3d2d6539503686fe71b1815f5e6611e1c627
Shashank1487 <shank1487@gmail.com>
2018-09-26 13:59:29 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+hbs.registerHelper("getFullYear", () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper("screamIt", text => {
+  return text.toUpperCase();
+});
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+app.use((req, resp, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("server.log", log + "\n", err => {
+    if (err) {
+      console.log("Could not append to server.log");
+    }
+  });
+  console.log(log);
+  next();
+});
+
+app.use((req, res, next) => {
+    console.log('Under Maintenance');
+  res.render("maintenance.hbs");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  //   res.send({
+  //     name: "Shashank",
+  //     likes: ["movies", "electronics"]
+  //   });
+  res.render("home.hbs", {
+    pageTitle: "Home Page",
+    welcomeMessage: "Welcome to Express and handlebar templating"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Page"
+  });
+});
+
+app.get("/bad", (req, res) => {
+  res.send({
+    errorMessage: "Unable to fulfill the request"
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is up at port 3000");
+});
